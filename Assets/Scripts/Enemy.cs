c73ed9c76d6289d5caadda0af55ee6ba327213a9
AntonioVelez24 <velez.fabriciotecsup.edu.pe>
2025-05-30 17:57:09 -0500

    arreglando el puño

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -14,28 +14,43 @@ public class Enemy : MonoBehaviour
     [SerializeField] private ColorsSO blackSO;
 
     private ColorsSO assignedColorSO;
+    private bool isBeingDestroyed = false; // Nueva bandera
 
     private void Start()
     {
         assignedColorSO = Random.value > 0.5f ? whiteSO : blackSO;
         spriteRenderer.color = assignedColorSO.currentColor;
     }
+
     private void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
     }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (isBeingDestroyed) return; // Ya está en proceso de destrucción
+
+        if (collision.gameObject.CompareTag("Punch"))
         {
+            Debug.Log("puñoooooooooo");
+            isBeingDestroyed = true;
+            Destroy(gameObject);
+        }
+        else if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("creceeeeee");
+            isBeingDestroyed = true;
             transform.DOScale(transform.localScale * scaleOnHit, scaleTime)
                      .OnComplete(() => Destroy(gameObject));
         }
     }
+
     public void Init(Transform target)
     {
         player = target;
     }
+
     public Color GetCurrentColor()
     {
         return assignedColorSO.currentColor;
